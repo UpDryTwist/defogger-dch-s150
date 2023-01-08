0101162b5e7cbd810e5456964e29c302a955db24
tathamg <tathamg@gmail.com>
2023-01-08 11:25:26 -0600

    Futzing wiht more scanning info.

--- bluescan.py
+++ bluescan.py
@@ -32,29 +32,31 @@ if __name__ == '__main__':
         print( f"Device {dev.addr} ({dev.addrType}), RSSI={dev.rssi} dB, connectable={dev.connectable}, updateCount={dev.updateCount}")
         for (adtype, desc, value) in dev.getScanData():
             print( f"  {desc} = {value} ({adtype})")
-        print (f"  Attempting to connect . . .")
-        peripheral = None
-        try:
-            peripheral = Peripheral(dev)
-            print( f"  ... connected.")
-            services = peripheral.getServices()
-            for service in services:
-                print(f"  {str(service)}:")
-                print(f"    Service UUID = {service.uuid}")
-                for characteristic in service.getCharacteristics():
-                    print(f"    {characteristic}, uuid={characteristic.uuid}, hnd={hex(characteristic.getHandle())}, supports {characteristic.propertiesToString()}")
-                    if characteristic.supportsRead():
-                        try:
-                            print(f"      -> {repr(characteristic.read())}")
-                        except BTLEException as e:
-                            print(f"      -> {e}")
-            print(f"Getting service by ID 0xd001")
-            service = peripheral.getServiceByUUID(0xd001)
-            print( f"    Service UUID = {service.uuid} FOUND" )
-            peripheral.disconnect()
-        except Exception as e:
-            if peripheral:
+        if dev.connectable:
+            print (f"  Attempting to connect . . .")
+            peripheral = None
+            try:
+                peripheral = Peripheral(dev)
+                print( f"  ... connected.")
+                services = peripheral.getServices()
+                for service in services:
+                    print(f"  {str(service)}:")
+                    print(f"    Service UUID = {service.uuid}")
+                    for characteristic in service.getCharacteristics():
+                        print(f"    {characteristic}, uuid={characteristic.uuid}, hnd={hex(characteristic.getHandle())}, supports {characteristic.propertiesToString()}")
+                        if characteristic.supportsRead():
+                            try:
+                                print(f"      -> {repr(characteristic.read())}")
+                            except BTLEException as e:
+                                print(f"      -> {e}")
+                print(f"Getting service by ID 0xd001")
+                service = peripheral.getServiceByUUID(0xd001)
+                print( f"    Service UUID = {service.uuid} FOUND" )
                 peripheral.disconnect()
-            print(f"Connection failed with {e}")
-
+            except Exception as e:
+                if peripheral:
+                    peripheral.disconnect()
+                print(f"  Connection failed with {e}")
+        else:
+            print( f"  not connectable, not trying")
 
